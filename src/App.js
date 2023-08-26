276a572b4312cde896f559111d46628a55697b13
JSNMGL <jmrodriguez.vb@gmail.com>
2023-08-26 17:14:35 +0800

    Frontend Training August 26

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Assets/styles/overrides.css';
+import 'Assets/styles/overrides.css';
 
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
@@ -11,6 +11,7 @@ const Home = lazy(() => import('./Pages/Home'));
 const Buttons = lazy(() => import('./Pages/Elements/Buttons'));
 const Forms = lazy(() => import('./Pages/Forms'));
 const Stripe = lazy(() => import('./Pages/Stripe'));
+const Jiboy = lazy(() => import('./Pages/Jiboy'));
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" exact element={<Home/>} />
+          <Route path="/jiboy" exact element={<Jiboy/>} />
           <Route path="/elements/buttons" exact element={<Buttons/>} />
           <Route path="/forms" exact element={<Forms/>} />
         </Routes>
