0bf8c5fd196e3b23fa94915cdcde4704bdfb728d
jmagnaye.vb <jmagnaye.vb@gmail.com>
2024-01-23 23:51:40 +0800

    import calendar components

--- src/Pages/SellerCenters.jsx
+++ src/Pages/SellerCenters.jsx
@@ -2,20 +2,31 @@ import React, { useEffect, useState } from 'react';
 import LayoutNoFooter from '../Components/Layout/LayoutNoFooter';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Button, Modal, Card } from 'react-bootstrap';
-import '../Assets/styles/Home/style.css'
 import ShopByCategory from 'Components/Shared/ShopByCategory';
 import { useCookies } from 'react-cookie';
+import '../Assets/styles/SellerCenter/style.css';
 import Sidebar from 'Components/Shared/Sidebar';
 
 
 const SellerCenter = (props) => {
-    const [cookies, setCookie, removeCookie] = useCookies(['currentUser', 'isLoggedIn', 'userDetails', 'userRole']);
-    const currentUser = cookies.currentUser;
+  const [cookies, setCookie, removeCookie] = useCookies(['currentUser', 'isLoggedIn', 'userDetails', 'userRole']);
+  const currentUser = cookies.currentUser;
 
   return (
     <LayoutNoFooter>
-      <Sidebar />
+      <Container>
+        <Row>
+          <Col className='mt-5'>
+            <span className='appointments'>Appointments</span>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <Sidebar /> */}
     </LayoutNoFooter>
+
+
+
   );
 };
 
