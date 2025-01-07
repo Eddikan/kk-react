b834eae3d21968b2422173edd0aa9fa9dd24b29d
eddikan <imeekwere15@gmail.com>
2025-01-07 23:24:15 +0100

    refactor: add toast to api service to handle errors

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -1,23 +1,36 @@
-// src/services/api.ts
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import toast from "react-hot-toast"; // Assuming you're using react-toastify for toasts
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_REACT_APP_API_ENDPOINT,
+  prepareHeaders: (headers) => {
+    const token = localStorage.getItem("kk-token");
+
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+// Create a custom baseQuery that catches errors
+const baseQueryWithErrorHandler = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error) {
+    // Show a toast for the error
+    result.error.data.errors.forEach((message) => {
+      toast.error(message);
+    });
+  }
+
+  return result;
+};
 
 export const api = createApi({
-  reducerPath: "api", // Unique key for the service
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_REACT_APP_API_ENDPOINT, // Base URL for all endpoints
-    prepareHeaders: (headers, { getState }) => {
-      // Add Authorization token if available
-      //   const token = (getState()).auth?.token;
-      const token = localStorage.getItem("kk-token");
-
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
-  tagTypes: ['Profile'],
-  endpoints: () => ({}), // Endpoints will be defined in specific services
+  reducerPath: "api",
+  baseQuery: baseQueryWithErrorHandler,
+  endpoints: () => ({}),
 });
 
 export default api;
