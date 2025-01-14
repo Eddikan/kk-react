0a79025a6442b3e3aee863f49c41e6a09c09cd7e
eddikan <imeekwere15@gmail.com>
2025-01-14 12:31:03 +0100

    fix: add route protection to api service

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -1,14 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import toast from "react-hot-toast"; // Assuming you're using react-toastify for toasts
+import localforage from "localforage";
 
+const logOut = async () => {
+  await localforage.clear();
+  localStorage.clear();
+  window.location.href = "/login";
+};
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_REACT_APP_API_ENDPOINT,
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("kk-token");
-
+    headers.set("Accept", `application/json`);
     if (token) {
       headers.set("Authorization", `Bearer ${token}`);
-      headers.set("Accept", `application/json`);
     }
     return headers;
   },
@@ -17,9 +22,16 @@ const baseQuery = fetchBaseQuery({
 // Create a custom baseQuery that catches errors
 const baseQueryWithErrorHandler = async (args, api, extraOptions) => {
   const result = await baseQuery(args, api, extraOptions);
-
   if (result.error) {
     // Show a toast for the error
+    console.log("errrr", result);
+    if (result.error.status == 401) {
+      // unauthorized
+      // logout
+      toast.error(result.error.data.message);
+      api.dispatch({ type: "RESET_STATE" });
+      logOut();
+    }
     result.error.data.errors.forEach((message) => {
       toast.error(message);
     });
