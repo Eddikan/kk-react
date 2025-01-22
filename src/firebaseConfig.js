889b42dc39e59b434cfb6de6b943bb9e22ef7d28
eddikan <imeekwere15@gmail.com>
2025-01-22 12:36:25 +0100

    refactor: update firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUTzitNFEIBR7cQjpcXYUCbKLZisvZUZ8",
-  authDomain: "kouture-konect.firebaseapp.com",
-  projectId: "kouture-konect",
-  storageBucket: "kouture-konect.appspot.com",
-  messagingSenderId: "528602575259",
-  appId: "1:528602575259:web:f272af180f3e953541b13e",
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
